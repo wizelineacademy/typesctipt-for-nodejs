fa04f79924a4e934a1c2576832a461a01ded9f2a
Alexis Aguirre Roldán <alexis.aguirre@wizeline.com>
2020-11-23 21:22:33 -0600

    Added .env file reading

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { initServer } from "./app";
 
-const PORT: number = 8080;
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+let PORT: number;
+// Retrieve the listening port from the environment variables
+PORT = Number(process.env.PORT);
+if (!PORT) {
+  PORT = 8080;
+}
+console.log("PORT");
 
 initServer(PORT);
