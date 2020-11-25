cfe12e6fbc75641428d187f11a8946849a0420be
helladyo <heladiofog@gmail.com>
2020-11-24 22:50:04 -0600

    Setting strict option to false in the ts config file.

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,11 @@ import { initServer } from './app/app.server';
 // HW message
 const halloMessage: string = 'Hallo Welt!';
 console.log(halloMessage);
+/**
+ * This issue is fixed because of the tsconfig file, I just removed the strict option or it can be st to false
+ */
 // Workaround in the meantime...Ask to tutors.
-const port: number = process.env.PORT ? +process.env.PORT : 3003;
+// const port: number = process.env.PORT ? +process.env.PORT : 3003;
+const port: number = +process.env.PORT || 3003;
 // Server initialization
 initServer(port);
