16c9c62b2ae02699dd27f8295885086e26453a26
noecl1 <noe.camacho@wizeline.com>
2020-11-25 02:29:45 -0600

    removed mock controllers

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import * as dotenv from 'dotenv';
 import morgan from 'morgan';
 import App from './app/app.server';
 
-// import PostsController from './controllers/posts/PostsController';
 dotenv.config();
 
 const app = new App({
   port: process.env.SERVER_PORT,
   controllers: [
-    // new PostsController(),
   ],
   middleWares: [
     bodyParser.json(),
