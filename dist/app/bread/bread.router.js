281f3170f6f92d4919b0bb0f389fbfb3af0a1586
oscar.castillo <oscar.castillo@wizeline.com>
2020-11-24 21:52:08 -0600

    Fix: Deleted dist files from repo

--- dist/app/bread/bread.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-exports.router = express_1.Router();
-exports.router.route('/').get((req, res) => {
-    res.json('Hello from Breads!');
-});
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const app_server_1 = require("./app/app.server");
-const port = +process.env.PORT;
-app_server_1.initServer(port);
