36af5b3c291a1aea46b38b1935dbb8b7617eb8df
Isaac JS <isaac.javascript@gmail.com>
2020-11-25 21:49:18 -0600

    Refactor project structure

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express , {Request, Response, Router} from 'express';
-
-export const router: Router = express.Router();
-
-router.get('/', (req: Request, res: Response) => {
-  res.render('index', {title: 'Express'});
-});
