96286ae2ab333ddb10ef84fcd10ae7ac7c5cf9e1
Gael Manzanares <manzanares.gael@gmail.com>
2020-11-27 22:04:09 -0600

    Create cakes.mock.ts

---
+++ src/utils/mocks/cakes.mock.ts
@@ -0,0 +1,20 @@
+export const cakesMocks = [
+    {
+        _id: 1,
+        name: "carrot cake",
+        description: "carrot cake",
+        ingredients: ["carrot", "sugar", "oil"],
+        price: 169,
+        stock: 15,
+        status: 'Available',
+    },
+    {
+        _id: 2,
+        name: "chocolate",
+        description: "chocolate cake",
+        ingredients: ["chocolate", "sugar", "milk"],
+        price: 99.99,
+        stock: 5,
+        status: 'LastUnits'
+    }
+];
