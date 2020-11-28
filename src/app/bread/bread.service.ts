17eee556f524a74cc38451597eeeb3d140ae2eb7
Erick Valdivia <alekzdev7@gmail.com>
2020-11-27 19:00:30 -0600

    Remove bread logic

--- src/app/bread/bread.service.ts
+++
@@ -1,8 +0,0 @@
-let bread = ['bread'];
-export const getBread = () => bread;
-
-export const makeBread = async (quantity: number): Promise<string[]> => {
-  bread = new Array(quantity).fill('bread');
-
-  return bread;
-};
