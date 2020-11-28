4a10b5d2738f65e86f37b1af710678ba9519862b
oscar.castillo <oscar.castillo@wizeline.com>
2020-11-27 21:23:14 -0600

    Added models as interfaces

---
+++ src/app/sale/sale.model.ts
@@ -0,0 +1,9 @@
+import { CakeInterface } from '../cake/cake.model';
+
+export interface SaleInterface {
+  customerName: string;
+  customerPhoneNumber: string;
+  customerEmail: string;
+  totalAmount: number;
+  cake: CakeInterface;
+}
