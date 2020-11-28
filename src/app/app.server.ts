dfa26c52cefebecdb3facf872d35ecb6b3f6509f
Erick Valdivia <alekzdev7@gmail.com>
2020-11-27 18:27:13 -0600

    Added sales route

--- src/app/app.server.ts
+++ src/app/app.server.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
-import { router as breadRouter } from './routes/bread.router';
-import { router as cakeRouter } from './routes/cake.router';
+import { router as breadRouter } from './bread/bread.router';
+import { router as cakeRouter } from './cake/cake.router';
+import { router as salesRouter } from './sales/sales.router';
 
 const app: Express = express();
 
 app.use('/breads', breadRouter);
 app.use('/cakes', cakeRouter);
+app.use('/sales', salesRouter);
 
 export const initServer = (port: number) => {
   app.listen(port, () => {
