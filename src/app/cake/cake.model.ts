fe984f60cd6d9f8121c4ba743388a702e4c882a8
oscar.castillo <oscar.castillo@wizeline.com>
2020-12-02 22:49:45 -0600

    Added validation to fields

--- src/app/cake/cake.model.ts
+++ src/app/cake/cake.model.ts
@@ -3,11 +3,17 @@ import { ICake } from './cake.interface';
 import { Status } from './status.enum';
 
 export const CakeSchema: Schema = new Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String, required: true },
-  ingredients: { type: [String], required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  description: { type: String, required: true, minlength: 50, maxlength: 1000 },
+  ingredients: { type: [String], required: true, minlength: 3 },
+  price: { type: Number, required: true, min: 1 },
+  quantity: { type: Number, required: true, min: 0 },
   status: { type: Status, required: true }
 });
 
--- src/app/sale/sale.model.ts
+++ src/app/sale/sale.model.ts
@@ -3,9 +3,19 @@ import { ISale } from './sale.interface';
 import { CakeSchema } from '../cake/cake.model';
 
 export const SaleSchema: Schema = new Schema({
-  customerName: { type: String, required: true },
-  customerPhoneNumber: { type: String, required: true },
-  customerEmail: { type: String },
+  customerName: { type: String, required: true, minlength: 3, maxlength: 50 },
+  customerPhoneNumber: {
+    type: String,
+    required: true,
+    minlength: 10,
+    match: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+  },
+  customerEmail: {
+    type: String,
+    minlength: 5,
+    maxlength: 1000,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  },
   totalAmount: { type: Number, required: true },
   cake: { type: CakeSchema, required: true }
 });
