798605d3b9e0ef37326cf24252dac2a7935b2f88
Gael Manzanares <manzanares.gael@gmail.com>
2020-12-22 20:14:55 -0600

    Update post.handler.ts

--- src/app/sales/handlers/post.handler.ts
+++ src/app/sales/handlers/post.handler.ts
@@ -1,21 +1,30 @@
-import { RequestHandler, Request, Response } from "express";
-import { createSale } from "../sale.service";
+import { RequestHandler, Request, Response, NextFunction } from "express";
+import { SaleService } from "../sale.service";
 import { Sale } from "../sale.class";
-import { Sale as SaleInterface } from "../sale.interface";
+import { ISale } from "../sale.interface";
 
-type Params = { saleId?: number };
+type Params = { saleId?: string };
 type Query = {};
-type Body = {};
+type Body = {
+    customerName: string,
+    customerPhoneNumber: string,
+    customerEmail: string,
+    quality: number,
+    cakeId: string,
+};
 type Req = Request< Params, {}, Body, Query >;
 type Res = Response;
+type Next = NextFunction;
 
 export const createHandler : RequestHandler[] = [
-    async(req: Req, res: Res) => {
-        const sale = req.body as SaleInterface;
-        const newSale = new Sale( sale );
-        console.log(newSale);
-        const saleCreated = await createSale(newSale);
-        res.json(saleCreated);
+    async(req: Req, res: Res, next: Next) => {
+        try{
+            const sale: Sale = new Sale(req.body);
+            const saleId = await sale.save();
+            res.json(saleId)
+        }catch(error){
+            next(error);
+        }
     }
 ]
 
