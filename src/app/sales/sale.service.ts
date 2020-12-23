ba292e088ebf83d74c933310d51bbe4c3ccefe63
Gael Manzanares <manzanares.gael@gmail.com>
2020-12-22 20:17:49 -0600

    Update sale.service.ts

--- src/app/sales/sale.service.ts
+++ src/app/sales/sale.service.ts
@@ -1,16 +1,27 @@
-import { salesMocks } from '../../utils/mocks/sales.mock';
-import { Sale as SaleInterface } from "../sales/sale.interface";
+//import { salesMocks } from '../../utils/mocks/sales.mock';
+import { Connection } from "mongoose";
+import { DataService } from "../../components/data-service.component";
+import { ISale } from "../sales/sale.interface";
+import { modelName } from "./sale.model";
+import { Sale } from "./sale.class";
 
+type SaleClass = Sale;
 
-export const createSale = (sale: {}) => {
-    return new Promise((resolve) => {
-        //console.log("from service sales:", sale);
-        const newSale = sale as SaleInterface;
-        salesMocks.push(newSale);
-        console.log({salesMocks});
-        setTimeout(() => {
-            resolve(newSale);
-        }, 1500);
-    });
+export class SaleService {
+    private dataService: DataService<ISale>;
+
+    constructor(connection: Connection){
+        this.dataService = new DataService(connection, modelName);
+    }
+
+    registerSale(sale: ISale): Promise<string>{
+        return this.dataService.insert(sale);
+    }
+
+    public async getSaleInstance(id: string){
+        const saleValues = await this.dataService.findById(id);
+        const sale: SaleClass = new Sale(saleValues);
+        return sale;
+    }
 }
 
