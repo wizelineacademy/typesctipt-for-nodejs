308960dc1157f073fd75672f101bbc363ed610e7
Gael Manzanares <manzanares.gael@gmail.com>
2020-12-22 20:17:00 -0600

    Create sale.model.ts

---
+++ src/app/sales/sale.model.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from "mongoose";
+
+export const modelName: string = "Sale";
+export const collection: string = "sales";
+
+model(modelName, new Schema({
+    customerName: Schema.Types.String,
+    customerPhoneNumber: Schema.Types.String,
+    customerEmail: Schema.Types.String,
+    totalAmount: Schema.Types.Number,
+    cakeId: Schema.Types.String
+}), collection);
