97a6f457217dda6a73e4d0ae7c4f3aa1992cf186
Gael Manzanares <manzanares.gael@gmail.com>
2020-12-22 20:19:24 -0600

    Update app.server.ts

--- src/app/app.server.ts
+++ src/app/app.server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Router } from "express";
 import {router as cakeRouter } from './cakes/cake.router';
 import {router as saleRouter } from './sales/sale.router';
+import { logErrors, clientErrorHandler } from '../utils/middlewares/errorHandler';
 
 const app: Express = express();
 const router: Router = Router();
@@ -11,9 +12,11 @@ router.use('/cake', cakeRouter);
 router.use('/sale', saleRouter);
 app.use(router);
 
+app.use(logErrors);
+app.use(clientErrorHandler);
+
 export function initServer(port: number){
     app.listen(port, function(){
         console.log('Server listen on port', port);
     });
 }
-
