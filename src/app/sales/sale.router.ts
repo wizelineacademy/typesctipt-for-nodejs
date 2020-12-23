67b1330bf18aa16db0cd11c04dec1adc830f9c59
Gael Manzanares <manzanares.gael@gmail.com>
2020-12-22 20:17:30 -0600

    Update sale.router.ts

--- src/app/sales/sale.router.ts
+++ src/app/sales/sale.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createHandler } from "./handlers/post.handler";
+import { getHandler } from "./handlers/get.handler";
 
 
 export const router: Router = Router();
 
-//router.route('/').get(getAllHandler);
+router.route('/:saleId').get(getHandler);
 router.route('/').post(createHandler);
