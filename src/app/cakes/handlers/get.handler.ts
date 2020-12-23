f8c9407d0f677c6495ad6d9fe6f3c2c2092d6e00
Gael Manzanares <manzanares.gael@gmail.com>
2020-12-22 20:09:00 -0600

    Update get.handler.ts

--- src/app/cakes/handlers/get.handler.ts
+++ src/app/cakes/handlers/get.handler.ts
@@ -1,23 +1,57 @@
-import { RequestHandler, Request, Response } from "express";
-import { getAllCakes, getCake } from "../cake.service";
+import { RequestHandler, Request, Response, NextFunction } from "express";
+import { CakeService } from "../cake.service";
+import { dbMain } from "../../app.database";
+import { ICakeFilter, ICake } from '../cake.interface';
 
-type Params = { cakeId?: number };
-type Query = {};
+type Params = { cakeId?: string };
+type Query = {
+    priceGreater?: number,
+    priceLesser?: number,
+    ingredients?: string[],
+};
 type Body = {};
 type Req = Request< Params, {}, Body, Query >;
 type Res = Response;
+type Next = NextFunction;
 
 export const getHandler: RequestHandler[] = [
-    async (req: Req, res: Res) => {
+    async (req: Req, res: Res, next: Next) => {
         const { cakeId } = req.params;
-        const cakes = await getCake(cakeId);
-        res.json(cakes);
+        const cakesService = new CakeService(dbMain);
+        try{
+            const cake = await cakesService.getCakeInstance(cakeId);
+            res.json(cake.getCakeDetails());
+        }catch(error){
+            next(error);
+        }
     }
 ]
 
 export const getAllHandler: RequestHandler[] = [
-    async (req: Req, res: Res) => {
-        const cakes = await getAllCakes();
-        res.json(cakes);
+    async (req: Req, res: Res, next: Next) => {
+        const cakeFilters: ICakeFilter = {};
+        
+        if(req.query.priceGreater){
+            if(!cakeFilters.price) cakeFilters.price = {};
+            cakeFilters.price.$gt = req.query.priceGreater;
+        }
+
+        if(req.query.priceLesser){
+            if(!cakeFilters.price) cakeFilters.price = {};
+            cakeFilters.price.$lt = req.query.priceLesser;
+        }
+
+        if(req.query.ingredients){
+            cakeFilters.ingredients = {};
+            cakeFilters.ingredients.$in = req.query.ingredients;
+        }
+
+        try{
+            const cakesService = new CakeService(dbMain);
+            const cakes = await cakesService.getAllCakes(cakeFilters);
+            res.json(cakes);
+        }catch(error){
+            next(error);
+        }
     }
 ]
